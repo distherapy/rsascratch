2bf1f6ae48db26269d17540cbe3a97a08d12db77
Brent Elisens <56242810+distherapy@users.noreply.github.com>
2024-08-18 19:30:18 -0400

    Update prng.py

--- prng.py
+++ prng.py
@@ -2,23 +2,21 @@ import random
 import math
 s = random.randint(9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999, 99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999)
 x = random.randint(1,6)
-y = ((2*x)/3)**3
-y = math.floor(y)
+y = math.floor(((2*x)/3)**3)
 o = s*y
 print("s =", s)
 print("y =", y)
 print("o =", o)
 p = []
-q = []
 p.append(o)
-def switch():	
-	for n in p:		
-		if n in q:
-			n = o - 1 
-			#? if seed repeats, -1...+2...-3.../-2x...
-		else:
-			q.append(n)	
-print("p =", p)
-random.shuffle(p)
-m = (random.choice(p))
-print("m =", m)
+
+# for n in p:
+	# if n in p:
+		# n = o - 1 #? if seed repeats, -1...+2...-3.../-2x...
+	# if n not in p:
+		# p.append(o)
+		# n = o - 1			
+# print("p =", p)
+# random.shuffle(p)
+# m = (random.choice(p))
+# print("m =", m * o)
